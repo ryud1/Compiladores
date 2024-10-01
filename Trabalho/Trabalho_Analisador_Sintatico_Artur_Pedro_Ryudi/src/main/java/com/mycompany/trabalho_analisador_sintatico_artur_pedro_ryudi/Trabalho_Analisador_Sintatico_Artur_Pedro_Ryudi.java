20691b893c55810be81f36c5de27feab44a7b613
turtur141 <arturobertosilva@gmail.com>
2024-09-30 22:47:45 -0300

    #teste
    teste commit

---
+++ Trabalho/Trabalho_Analisador_Sintatico_Artur_Pedro_Ryudi/src/main/java/com/mycompany/trabalho_analisador_sintatico_artur_pedro_ryudi/Trabalho_Analisador_Sintatico_Artur_Pedro_Ryudi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.trabalho_analisador_sintatico_artur_pedro_ryudi;
+import javax.swing.JFrame;
+/**
+ *
+ * @author Bebeto
+ */
+public class Trabalho_Analisador_Sintatico_Artur_Pedro_Ryudi {
+
+    public static void main(String[] args) {
+        Tela tela = new Tela();
+        tela.setVisible(true);
+    }
+}
